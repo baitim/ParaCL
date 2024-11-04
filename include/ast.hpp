c3d5aeebf37a0d889bf62449e0a33b66ef23091a
Тимур Байдюсенов <63646703+baitim@users.noreply.github.com>
2024-11-04 18:53:32 +0300

    add check root == nullptr

--- include/ast.hpp
+++ include/ast.hpp
@@ -5,6 +5,10 @@
 namespace ast {
     struct ast_t final {
         node::node_t* root_ = nullptr;
-        int execute() { return root_->execute(); }
+        int execute() {
+            if (root_)
+                return root_->execute();
+            throw "execute by nullptr";
+        }
     };
 }
