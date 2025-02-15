b3f17a12937552e7f5c51be718c7c6af124fe7fb
Timur Baidusenov <baidiusenov.tb@phystech.edu>
2025-02-15 03:14:32 +0300

    flex to workflow

--- conanfile.py
+++ conanfile.py
@@ -17,7 +17,6 @@ class paraclRecipe(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
-    requires = "flex/2.6.4"
     test_requires = "gtest/1.15.0"
 
     # Sources are located in the same place as this recipe, copy them to the recipe
